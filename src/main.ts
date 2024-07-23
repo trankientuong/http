07b2543168d7f2bd69c1dc590e5502c34ba05680
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-23 17:30:42 +0700

    Handling http response interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
   // const req = request.clone({
@@ -9,7 +10,17 @@ function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) 
   // });
   console.log('[Outgoing Request]');
   console.log(request);
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, { providers: [provideHttpClient(
